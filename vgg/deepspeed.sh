ed9d63809bbe3d19dea38546433e057759d05dc9
devd <devd@node0.devd-253978.uwmadison744-s25-pg0.wisc.cloudlab.us>
2025-04-21 21:00:41 +0000

    added dp

--- vgg/deepspeed.sh
+++ vgg/deepspeed.sh
@@ -1,6 +1,11 @@
-deepspeed --num_nodes=1 --num_gpus=2\
- --master_port=12345 traindeepspeed.py\
-  -a vgg16 --deepspeed --deepspeed_config /scr/dmehrotra/CS744-Project/config/ds_config.json \
-  --dummy \
-  -b 64 \
---epochs 5 \
+deepspeed --num_nodes=2 --num_gpus=4 \
+  --node_rank=0 \
+  --master_addr=10.10.1.1 \
+  --master_port=12345 \
+  traindeepspeed.py \
+    -a vgg16 \
+    --deepspeed \
+    --deepspeed_config /config/ds_config.json \
+    --dummy \
+    -b 64 \
+    --epochs 5
