19d8d99e6730225aa3011cae6b45c1eefc055038
devd <devd@node0.devd-253978.uwmadison744-s25-pg0.wisc.cloudlab.us>
2025-04-21 21:25:51 +0000

    added cofig

--- vgg/deepspeed.sh
+++ vgg/deepspeed.sh
@@ -1,8 +1,12 @@
 deepspeed --hostfile hostfile.txt \
+  --no_ssh \
+  --node_rank 0 \
+  --master_addr 10.10.1.1 \
+  --master_port 12345 \
   traindeepspeed.py \
   -a vgg16 \
   --deepspeed \
-  --deepspeed_config /config/ds_config.json \
+  --deepspeed_config /users/devd/CS744-Project/vgg/config/ds_config2.json \
   --dummy \
   -b 64 \
   --epochs 5
