a1de9959ac2da6fbfe4cb783b831e8bc4e2f1cd3
devd <devd@node0.devd-253978.uwmadison744-s25-pg0.wisc.cloudlab.us>
2025-04-21 21:05:25 +0000

    Added hostfile

--- vgg/deepspeed.sh
+++ vgg/deepspeed.sh
@@ -1,11 +1,8 @@
-deepspeed --num_nodes=2 --num_gpus=4 \
-  --node_rank=0 \
-  --master_addr=10.10.1.1 \
-  --master_port=12345 \
+deepspeed --hostfile hostfile.txt \
   traindeepspeed.py \
-    -a vgg16 \
-    --deepspeed \
-    --deepspeed_config /config/ds_config.json \
-    --dummy \
-    -b 64 \
-    --epochs 5
+  -a vgg16 \
+  --deepspeed \
+  --deepspeed_config /config/ds_config.json \
+  --dummy \
+  -b 64 \
+  --epochs 5
